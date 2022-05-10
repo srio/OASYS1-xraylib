5a02e21ed93c3c3afe39a099700be4b8e0ce2172
Manuel Sanchez del Rio <srio@esrf.eu>
2022-05-10 10:32:50 +0200

    Update go.sh

--- go.sh
+++ go.sh
@@ -3,7 +3,6 @@ git clone https://github.com/tschoonj/xraylib
 cd xraylib
 git checkout mesonpep517
 python -m pip wheel .
-#python -m pip install xraylib-4.1.1-cp37-cp37m-linux_x86_64.whl
-
 auditwheel repair xraylib-4.1.1-cp37-cp37m-linux_x86_64.whl --plat "manylinux2014_x86_64" -w wheels
+python -m twine upload wheels/xraylib-4.1.1-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
 
